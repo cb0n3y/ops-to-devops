74a64ced5ab2baf7427c3cd2ea6b1802e8010abf
cb0n3y <cb0n3y@gmail.com>
2025-06-04 19:08:35 +0200

    chore: fix syntax, unify shared provisioning, add secure env usage
    Signed-off-by: cb0n3y <cb0n3y@gmail.com>

---
+++ iaac/vagrant/monitoring/grafana/scripts/graf-setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set e
+
+PROM_SRV="xxx.xxx.xxx.xxx"
+GRAF_SRV="xxx.xxx.xxx.xxx"
+PORTS=(3000 9100)
+
+create_repository() {
+    echo -e "\n[+] Creating Prometheus repository ..."
+
+    if [[ -e "$TARGET_FILE" ]]; then
+        echo "The repo file already exists."
+    else
+        cat > "$TARGET_FILE" <<EOF
+[prometheus]
+name=prometheus
+baseurl=https://packagecloud.io/prometheus-rpm/release/el/\$releasever/\$basearch
+repo_gpgcheck=1
+enabled=1
+gpgkey=https://packagecloud.io/prometheus-rpm/release/gpgkey
+        https://raw.githubusercontent.com/lest/prometheus-rpm/master/RPM-GPG-KEY-prometheus-rpm
+gpgcheck=1
+metadata_expire=300
+EOF
+    fi
+
+    echo -e "\n[+] Repository created"
+}
+
+create_hosts_file() {
+    echo -e "\n[+] Generating hosts file for DNS resolution..."
+
+    echo "" >> /etc/hosts
+    echo "$PROM_SRV     dev-prometheus01.fritz.box  dev-prometheus01" >> /etc/hosts
+    echo "$GRAF_SRV     dev-grafana01.fritz.box  dev-grafana01" >> /etc/hosts
+}
+
+install_node_exporter() {
+    echo -e "\n[+] Installing node-exporter..."
+
+    dnf -y update
+    dnf -y install node_exporter
+    systemctl enable --now node_exporter.service
+}
+
+create_repository
+install_node_exporter
+create_hosts_file
---
+++ iaac/vagrant/shared-scripts/set_fw_rules.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set e
+
+set_fw_rules() {
+    # Use ports from environment variable or fall back to default
+    IFS=' ' read -r -a ports <<< "${PORTS:-3000 9100}"
+
+    echo -e "\n[+] Adding required firewall rules for ports: ${ports[*]}"
+
+    # Check if firewalld is running
+    if ! systemctl is-active --quiet firewalld; then
+        echo "firewalld is not running. Attempting to start..."
+        systemctl start firewalld
+        systemctl enable firewalld
+    fi
+
+    # Add each port to firewalld
+    for port in "${ports[@]}"; do
+        firewall-cmd --permanent --add-port="$port"/tcp
+    done
+
+    echo "[*] Reloading firewalld..."
+    firewall-cmd --reload
+
+    echo -e "\n[+] Firewall rules successfully configured."
+}
+
+set_fw_rules
