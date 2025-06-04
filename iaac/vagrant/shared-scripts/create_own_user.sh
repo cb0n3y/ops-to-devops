0be861c68891d14b67420b6de2410db7c046ad69
cb0n3y <cb0n3y@gmail.com>
2025-06-04 21:36:16 +0200

    chore(shared-scripts): fix create_own_user.sh
    Signed-off-by: cb0n3y <cb0n3y@gmail.com>

--- iaac/vagrant/shared-scripts/create_own_user.sh
+++ iaac/vagrant/shared-scripts/create_own_user.sh
@@ -6,11 +6,17 @@ set -e
 USER_NAME="${USER_NAME:-}"
 COMMENT="${COMMENT:-}"
 
+
+if [[ -z "$USER_NAME" ]]; then
+  echo "ERROR: USER_NAME is not set. Exiting."
+  exit 1
+fi
+
 create_user() {
     echo -e "\n[+] Creating required user..."
 
     if id "$USER_NAME" &>/dev/null; then
-        echo "User cb0n3y already exists, skipping creation."
+        echo "User $USER_NAME already exists, skipping creation."
     else
         useradd -m -c "$COMMENT" -s /bin/bash "$USER_NAME"
         usermod -a -G wheel "$USER_NAME"
